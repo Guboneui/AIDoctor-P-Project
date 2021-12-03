2a96b4eb960af628b2bcf87279d19567ca5b4676
Guboneui <starku2249@naver.com>
2021-12-04 04:40:08 +0900

    설정화면 추가

--- AIDoctor-P Project/View/Main/ThirdViewController.swift
+++ AIDoctor-P Project/View/Main/ThirdViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var chatSwitch: UISwitch!
+    @IBOutlet weak var alertSwitch: UISwitch!
+    
+    override func loadView() {
+        super.loadView()
+        
+        logoutButton.layer.cornerRadius = 20
+        chatSwitch.transform = CGAffineTransform(scaleX: 0.75, y: 0.75)
+        alertSwitch.transform = CGAffineTransform(scaleX: 0.75, y: 0.75)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ AIDoctor-P Project/View/Setting/SettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingViewController.swift
+//  AIDoctor-P Project
+//
+//  Created by 구본의 on 2021/12/04.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
