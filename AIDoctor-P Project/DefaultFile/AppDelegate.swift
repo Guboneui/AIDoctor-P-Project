f1eb14c482304a52405a8159d93c47f4f387022f
Guboneui <starku2249@naver.com>
2021-12-14 01:08:45 +0900

    FCM 연동

--- AIDoctor-P Project/DefaultFile/AppDelegate.swift
+++ AIDoctor-P Project/DefaultFile/AppDelegate.swift
@@ -8,13 +8,15 @@
 import UIKit
 import IQKeyboardManager
 import Firebase
-import FirebaseMessaging
 import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
+    let gcmMessageIDKey = "gcm.Message_id"
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Thread.sleep(forTimeInterval: 0.5)
@@ -32,19 +34,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         IQKeyboardManager.shared().accessibilityScroll(.up)
         
         FirebaseApp.configure()
+        
+        // [START set_messaging_delegate]
         Messaging.messaging().delegate = self
-        UNUserNotificationCenter.current().delegate = self
+        // [END set_messaging_delegate]
+        // Register for remote notifications. This shows a permission dialog on first run, to
+        // show the dialog at a more appropriate time move this registration accordingly.
+        // [START register_for_notifications]
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: { _, _ in }
+            )
+        } else {
+            let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        
         
         return true
     }
-    
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
-        print("[Log] deviceToken :", deviceTokenString)
-        
-        Messaging.messaging().apnsToken = deviceToken
-    }
-    
     
     
     
@@ -63,41 +79,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     
-    
-    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
 }
-
-
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        /// 앱이 foreground  상태일 때 Push 받으면 alert를 띄워준다
         if #available(iOS 14.0, *) {
-            completionHandler([.badge, .sound])
+            completionHandler([.banner, .sound])
+            print("14이상")
         } else {
-            completionHandler([.alert, .badge, .sound])
+            completionHandler([.alert, .sound])
+            print("14이하")
         }
-        
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        completionHandler()
     }
 }
+
 
 
 extension AppDelegate: MessagingDelegate {
+    
+    
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        
         let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
         
-        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
+        AIDoctorLog.debug("Firebase registration token: \(fcmToken ?? "")")
+        AIDoctorLog.debug(NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        ))
         
-        
-        Messaging.messaging().token { token, error in
-            if let error = error {
-                AIDoctorLog.debug("Error fetching FCM registration token: \(error)")
-            } else if let token = token {
-                AIDoctorLog.debug("기기 FCM 토큰 정보: \(token)")
-            }
-        }
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
     }
+
 }
+
